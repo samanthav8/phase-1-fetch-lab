3781061b8a51bd5e1919d917fb05c65a8aeb6546
Samantha <samanthavillafana8@gmail.com>
2024-01-02 22:01:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
   .then((json) => renderBooks(json))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
